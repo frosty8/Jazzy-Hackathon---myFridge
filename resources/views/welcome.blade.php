7580dd4dfafaa24d15d16b87d4166949b77f2563
rusinArek <arek.rusinek94@gmail.com>
2017-05-27 10:05:40 +0200

    Body

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,17 @@
-HELLO
+<html>
+<head>
+    <title>My fridge</title>
+    <link href="{!! asset('css/welcome.css') !!}" media="all" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="{!! asset('js/app.min.js') !!}"></script>
+</head>
+<body>
+
+<div>
+    <div id="title">
+        <font>MyFridge</font>
+
+    </div>
+</div>
+</div>
+</body>
+</html>
